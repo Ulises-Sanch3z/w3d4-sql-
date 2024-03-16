4f5888c957dd228929cc456dd7ba1b5c5a98f400
Ulises_Sanch3z <chino287a@gmail.com>
2024-03-16 10:42:02 -0400

    Initial commit

---
+++ js_sql/app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import { Sequelize, DataTypes } from 'sequelize';
+import pkg from 'sequelize';
+const { Model } = pkg;
+
+const sequelize = new Sequelize('sqlite::memory:');
+
+const app = express();
+app.use(express.json());
+
+class User extends Model {}
+User.init({
+    username: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+}, { sequelize, modelName: 'user' });
+
+class Post extends Model {}
+Post.init({
+    title: { type: DataTypes.STRING, allowNull: false },
+    content: { type: DataTypes.TEXT, allowNull: false },
+}, { sequelize, modelName: 'post' });
+
+class Comment extends Model {}
+Comment.init({
+    content: { type: DataTypes.TEXT, allowNull: false },
+}, { sequelize, modelName: 'comment' });
+
+User.hasMany(Post);
+Post.belongsTo(User);
+User.hasMany(Comment);
+Post.hasMany(Comment);
+Comment.belongsTo(User);
+Comment.belongsTo(Post);
+
+app.post('/posts', async (req, res) => {
+    const { title, content, userId } = req.body;
+    try {
+        const post = await Post.create({ title, content, userId });
+        res.status(201).json(post);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+sequelize.sync().then(() => {
+    app.listen(3000, () => console.log('Server running on port 3000'));
+});
